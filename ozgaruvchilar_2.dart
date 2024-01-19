9ec371f17cfcc75425b6d23fbc3a83b62a4f5a1e
Sunnat Amirov <sunnatbekamirov22@gmail.com>
2024-01-19 09:09:52 +0500

    Ma'lumot turlari 2-qism.

---
+++ ozgaruvchilar_2.dart
@@ -0,0 +1,29 @@
+bool eshikOchiqmi = false;
+num son = 5;
+// List<num> sonlar = [1, 4, 1, 85, 41.5, -45];
+
+void main() {
+  // print("Eshikni ochiqmi: $eshikOchiqmi");
+  // print(sonlar);
+  // List<int> sonlar = [
+  //   1,
+  //   5,
+  //   8,
+  //   11,
+  //   5,
+  //   69,
+  //   87,
+  //   4
+  // ]; // sonlardan tashkil topgan ro'yxat
+  // print("Ro'yxatdagi sonlar: $sonlar");
+  // print("Ro'yxatdagi birinchi son: ${sonlar[0]}"); // index o dan boshlanadi
+  // print("Ro'yxatdagi ikkinchi element: ${sonlar[1]}");
+  // print("Ro'yxatdagi beshinchi element: ${sonlar[4]}");
+  // print("O'ninchi element: ${numbers[9]}"); // RangeError
+
+  // Set<String> ismlar = {"Sunnat", "Jim", "Pam", "Sunnat"};
+  // print(ismlar);
+
+  Map<String, dynamic> lugat = {"Sunnat": 19, "Pam": 30, "Sunnat": 14};
+  print(lugat);
+}
