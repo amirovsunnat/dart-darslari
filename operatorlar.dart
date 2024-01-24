b964d0429f7c73de80ced5252fee771c440106b7
Sunnat Amirov <sunnatbekamirov22@gmail.com>
2024-01-24 09:30:03 +0500

    Operatorlar.

---
+++ operatorlar.dart
@@ -0,0 +1,197 @@
+// num son1 = 5;
+// num son2 = 14;
+
+void main(List<String> args) {
+  // print(son1 + son2);
+  // print(son1 - son2);
+  // var son3 = -son1;
+  // print(son3);
+  // print(son1);
+
+  // print(son1 * son2);
+  // print(son1 / son2);
+  // print(son2 / son1);
+  // print(son2 ~/ son1);
+  // print(son2 % son1);
+
+  // num son1 = 15;
+  // num son2 = 2.5;
+
+  // // arithmetic operations
+  // var yigindi = son1 + son2; // addition (qo'shish)
+  // var ayirma = son1 - son2; // subtraction (ayirish)
+  // var kopaytma = son1 * son2; // multiplication (ko'paytirish)
+  // var bolinma = son1 / son2; // division (bo'lish)
+  // var butunBolinma = son1 ~/ son2; // integer division (butun bo'lish)
+  // var qoldiq =
+  //     son1 % son2; // modulus -- Remainder After Integer Division (qoldiq)
+  // var unaryMinus =
+  //     -son1; // unary minus -- For reversing the sign of the expression (ishorani teskarisiga o'zgartirish)
+
+  // print("Ikki sonning yig'indisi -- $yigindi");
+  // print(
+  //     "******************************************"); // oq'ishga onson bo'lishligi uchun
+  // print("Ikki sonning ayirmasi -- $ayirma");
+  // print(
+  //     "******************************************"); // oq'ishga onson bo'lishligi uchun
+  // print("IKKI SONNING KO'PAYTMASI -- $kopaytma");
+  // print(
+  //     "******************************************"); // oq'ishga onson bo'lishligi uchun
+  // print("Ikki sonning bo'linmasi --  $bolinma");
+  // print(
+  //     "******************************************"); // oq'ishga onson bo'lishligi uchun
+  // print("Butun bo'linma --  $butunBolinma");
+  // print(
+  //     "******************************************"); // oq'ishga onson bo'lishligi uchun
+  // print("Birinchi sonni ikkinchi songa bo'lgandagi qoldiq --  $qoldiq");
+  // print(
+  //     "******************************************"); // oq'ishga onson bo'lishligi uchun
+  // print("Birinchi songa qarama-qarshi son -- $unaryMinus");
+  // print(
+  //     "******************************************"); // oq'ishga onson bo'lishligi uchun
+
+  // int son1 = 5;
+  // int son2 = 10;
+
+  // print("Son1 ning asl qiymati --  $son1");
+  // print("Son2 ning asl qiymati --  $son2");
+  // print(
+  //     "******************************************"); // o'qishga onson bo'lishlik uchun
+
+  // // pre increment (oldin oshirish)
+
+  // son1 = ++son1; // qiymatni 1 ga oshiradi
+  // son2 = ++son2;
+
+  // print("1 ga oshirgandan keyingi Son1 ning qiymati $son1");
+  // print("1 ga oshirgandan keyingi Son2 ning qiymati$son2");
+  // print(
+  //     "******************************************"); // o'qishga onson bo'lishlik uchun
+
+  // // post increment (keyin oshirish)
+  // son1 = son1++;
+  // son2 = son2++; // qiymatni birga oshiradi
+
+  // print("Son1 ning qiymatini post incrementdan keyin qiymati $son1");
+  // print("Son2 ning qiymatini post incrementdan keyin qiymati $son2");
+  // print(
+  //     "******************************************"); // o'qishga onson bo'lishlik uchun
+
+  // son1 = --son1; // qiymatni birga kamaytiradi
+  // son2 = --son2;
+
+  // print("1 ga kamaytirgandan keyingi son1 ning qiymati $son1");
+  // print("1 ga kamaytirgandan keyingi son2 ning qiymati $son2");
+  // print(
+  //     "******************************************"); // o'qishga onson bo'lishlik uchun
+
+  // son1 = son1--;
+  // son2 = son2--; // qiymatni birga kamaytirish
+
+  // print("Son1 ni 1 ga post decrement qilgandan keyingi qiymati $son1");
+  // print("Son2 ni 1 ga post decrement qilgandan keyingi qiymati $son2");
+  // print(
+  //     "******************************************"); // o'qishga onson bo'lishlik uchun
+
+  // int yosh = 20;
+  // print(yosh);
+  // yosh += 7;
+  // print(yosh);
+  // yosh -= 8;
+  // print(yosh);
+
+  // yosh *= 2;
+  // print(yosh);
+
+  // double son = 30;
+  // print(son);
+  // son /= 3;
+  // print(son);
+
+  // Assignment Operators
+
+  // num son = 14;
+  // son += 10; //  songa 10 ni qo'shadi
+  // print("Songa 10 qo'shgandan keyin qiymati -- $son");
+  // print("******************************************"); // o'qishga onson bo'lishlik uchun
+  // son -= 2; // sondan 2 ni ayirish
+  // print("Sondan 2 ni ayirgandan keyingi qiymati -- $son");
+  // print("******************************************"); // o'qishga onson bo'lishlik uchun
+  // son *= 3; // 3 ga ko'paytirish
+  // print("3 ga ko'paytirgandan keyingi qiymati = $son");
+  // print("******************************************"); // o'qishga onson bo'lishlik uchun
+  // son /= 5; // divide number to 5
+  // print("$son ni 5 ga bo'lgandan keyingi qiymati = $son");
+  //   print("******************************************"); // o'qishga onson bo'lishlik uchun
+
+  int talabaId = 0228035;
+  int passportId = 1452876;
+
+  // print((talabaId == 0228035) && (passportId == 1452876));  // && -- and
+  // print(
+  //     "******************************************"); // o'qishga onson bo'lishlik uchun
+
+  // print((talabaId == 845122) &&
+  //     (passportId == 1452876)); // false because first condition is false
+  // print(
+  //     "******************************************"); // o'qishga onson bo'lishlik uchun
+
+  // print((talabaId == 845122) || (passportId == 1452876)); //
+  // print(
+  //     "******************************************"); // o'qishga onson bo'lishlik uchun
+
+  // print((talabaId == 0228035) != (passportId == 1452876)); //
+  // print(
+  //     "******************************************"); // o'qishga onson bo'lishlik uchun
+
+  // String ism = "Isaac";
+  // int yosh = 55;
+
+  // print(
+  //     // ignore: unnecessary_type_check
+  //     "Ism String ma'lumot turimi?: ${ism is String}"); // true qaytaradi agar holat to'g'ri bo'lsa
+  // print(
+  //     "******************************************"); // o'qishga onson bo'lishligi uchun
+
+  // print(
+  //     "ism String emasmi? : ${ism is! String}"); // false qaytaradi agar holat to'g'ri bo'lsa
+  // print(
+  //     "******************************************"); // o'qishga onson bo'lishligi uch
+
+  // print("Isaacning yoshi int ma'lumot turidami? : ${yosh is int}");
+  // print(
+  //     "******************************************"); // o'qishga onson bo'lishligi uch
+
+  var five = 5;
+  var ten = 10;
+
+  print(
+      "5 soni 10 sonidan kattami: ${five > ten}"); // katta-- Qaysi operand kattaligini tekshiradi va natijani boolean ma’lumot turida qaytaradi.
+  print(
+      "******************************************"); // o'qishga onsoni bo'lishligi uchun
+
+  print(
+      "5 soni 10 dan kichkinami: ${five < ten}"); // kichik	-- Qaysi operand kichikligini tekshiradi va natijani boolean ma’lumot turida qaytaradi.
+  print(
+      "******************************************"); // o'qishga onsoni bo'lishligi uchun
+
+  print(
+      "5 soni 10 dan katta yoki tengmi: ${five >= ten}"); // Qaysi operand kattaligini yoki tengligini tekshiradi va natijani boolean ma’lumot turida qaytaradi.
+  print(
+      "******************************************"); // o'qishga onsoni bo'lishligi uchun
+
+  print(
+      "5 soni 10 dan kichik yoki tengmi: ${five <= ten}"); // Less than or equal to	-- Used to check which operand is smaller or equal and gives result as boolean
+  print(
+      "******************************************"); // o'qishga onsoni bo'lishligi uchun
+
+  print(
+      "5 bilan 10 tengmi?: ${five == ten}"); // Equal to	-- Used to check operands are equal to each other and gives result as boolean
+  print(
+      "******************************************"); // o'qishga onsoni bo'lishligi uchun
+
+  print(
+      "5 bilan 10 bir-biriga teng emasmi?: ${five != ten}"); // Operandlar bir-biriga teng emasligini tekshiradi va natijani boolean ma’lumot turida qaytaradi.
+  print(
+      "******************************************"); // o'qishga onsoni bo'lishligi uchun
+}
