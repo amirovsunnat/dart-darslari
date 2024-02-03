be794eb66f2c5dfc2ca0dc52bc269c84b938707d
Sunnat Amirov <sunnatbekamirov22@gmail.com>
2024-02-03 18:34:09 +0500

    Conditions.

---
+++ conditions.dart
@@ -0,0 +1,57 @@
+void main() {
+  // int yosh = 18;
+  // if (yosh >= 16) {
+  //   // agar yoshimiz 16 dan katta yoki teng bo'lganda "Siz ...." chop et.
+  //   print("Siz passport olishingiz mumkin");
+  // } else {
+  //   print("Siz passport olishingiz mumkin emas.");
+  // }
+  // bool dasturchimi = false;
+  // if (dasturchimi == true) {
+  //   print("Siz senior dasturchisiz");
+  // } else {
+  //   print("Siz dasturchi emassiz");
+  // }
+
+  // int yosh = 17;
+  // if (yosh <= 12) {
+  //   print("10000 so'm");
+  // } else if (yosh > 12 && yosh <= 16) {
+  //   print("12000 so'm");
+  // } else if (yosh > 16 || yosh <= 18) {
+  //   print("14000 so'm");
+  // } else {
+  //   print("15000 so'm");
+  // }
+
+  int oyRaqami = 13;
+
+  // Oy raqamini tekishirish
+  if (oyRaqami == 1) {
+    print("Yanvar oyi");
+  } else if (oyRaqami == 2) {
+    print("Fevral oyi");
+  } else if (oyRaqami == 3) {
+    print("Mart oyi");
+  } else if (oyRaqami == 4) {
+    print("Aprel oyi");
+  } else if (oyRaqami == 5) {
+    print("May oyi");
+  } else if (oyRaqami == 6) {
+    print("Iyun oyi");
+  } else if (oyRaqami == 7) {
+    print("Iyul oyi");
+  } else if (oyRaqami == 8) {
+    print("Avgust oyi");
+  } else if (oyRaqami == 9) {
+    print("Sentabr oyi");
+  } else if (oyRaqami == 10) {
+    print("Oktabr oyit");
+  } else if (oyRaqami == 11) {
+    print("Noyabr oyi");
+  } else if (oyRaqami == 12) {
+    print("Dekabr oyi");
+  } else {
+    print("Bu raqamdagi oy mavjud emas!");
+  }
+}
