cac41b84bc38b9ad155fc0797441a602cfeb3f7d
Sunnat Amirov <sunnatbekamirov22@gmail.com>
2024-02-06 18:57:10 +0500

    Switch case.

---
+++ switch.dart
@@ -0,0 +1,78 @@
+void main() {
+  // var haftaKuni = 14;
+  // if (haftaKuni == 1) {
+  //   print("Bugun Dushanba.");
+  // } else if (haftaKuni == 2) {
+  //   print("Bugun Seshanba.");
+  // } else if (haftaKuni == 3) {
+  //   print("Bugun Chorshanba.");
+  // } else if (haftaKuni == 4) {
+  //   print("Bugun Payshanba.");
+  // } else if (haftaKuni == 5) {
+  //   print("Bugun Juma.");
+  // } else if (haftaKuni == 6) {
+  //   print("Bugun Shanba.");
+  // } else if (haftaKuni == 7) {
+  //   print("Bugun Yakshanba.");
+  // } else {
+  //   print("Mavjud bo'lmagan hafta kuni.");
+  // }
+  // var haftaKuni = 14;
+
+  // switch (haftaKuni) {
+  //   case 1:
+  //     print("Dushanba");
+  //     break;
+  //   case 2:
+  //     print("Seshanba");
+  //     break;
+  //   case 3:
+  //     print("Bugun Chorshanba.");
+  //     break;
+  //   case 4:
+  //     print("Bugun Payshanba.");
+  //     break;
+  //   case 5:
+  //     print("Bugun Juma.");
+  //     break;
+  //   case 6:
+  //     print("Bugun Shanba.");
+  //     break;
+  //   case 7:
+  //     print("Bugun Yakshanba.");
+  //     break;
+  //   default:
+  //     print("Bunaqangi hafta kuni mavjud emas");
+  // }
+
+  final baho = '5';
+
+  switch (baho) {
+    case 'A++':
+      print("Sizning bahoingiz $baho");
+      break;
+    case 'A+':
+      print("Sizning bahoingiz $baho");
+      break;
+    case 'A':
+      print("Sizning bahoingiz $baho");
+      break;
+    case 'B+':
+      print("Sizning bahoingiz $baho");
+      break;
+    case 'B':
+      print("Sizning bahoingiz $baho");
+      break;
+    case 'C+':
+      print("Sizning bahoingiz $baho");
+      break;
+    case 'C':
+      print("Sizning bahoingiz $baho");
+      break;
+    case 'D':
+      print("Sizning bahoingiz $baho");
+      break;
+    default:
+      print("Kitilgan baho to'g'ri formatda emas.");
+  }
+}
