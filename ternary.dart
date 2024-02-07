0f583e3ff17a4a346cd6f4186b0db0ef40f0a65a
Sunnat Amirov <sunnatbekamirov22@gmail.com>
2024-02-07 11:07:30 +0500

    Ternary operators.

---
+++ ternary.dart
@@ -0,0 +1,35 @@
+void main(List<String> args) {
+  // int son1 = 20;
+  // int son2 = 20;
+
+  // if (son1 > son2) {
+  //   print("Son1 son2 dan katta");
+  // } else if (son2 > son1) {
+  //   print("Son2 son1 dan katta");
+  // } else {
+  //   print("uLAR teng");
+  // }
+
+  // son1 > son2
+  //     ? print("Son1 son2 dan katta")
+  //     : son1 == son2
+  //         ? print("uLAR teng")
+  //         : print("Son2 son1 dan katta");
+
+  // int son1 = 20;
+  // int son2 = 15;
+  // int kattaRaqam = (son1 > son2) ? son1 : son2;
+  // print("Katta son $kattaRaqam");
+
+  // // ikkinchi misol
+  // var tanlov = 7;
+  // var javob = (tanlov == 1) ? "Yutdingiz" : "Yutqazdingiz";
+  // print(javob);
+
+  // uchinchi misol
+  int yosh = 17;
+  var tekhiruvNatijasi = (yosh >= 18)
+      ? "Siz mashina haydashingiz mumkin."
+      : "Sizga mashina haydashingiz mumkin emas.";
+  print(tekhiruvNatijasi);
+}
