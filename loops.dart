d8b82cdffaad3c95707147043760981e09dfd568
Sunnat Amirov <sunnatbekamirov22@gmail.com>
2024-02-08 09:53:04 +0500

    For loop.

---
+++ loops.dart
@@ -0,0 +1,49 @@
+void main() {
+  // print("Temur");
+  // print("Temur");
+  // print("Temur");
+  // print("Temur");
+  // print("Temur");
+  // print("Temur");
+  // print("Temur");
+  // print("Temur");
+  // print("Temur");
+  // print("Temur");
+
+  // for (int i = 1; i < 10; i++) {
+  //   print("$i Temur");
+  // }
+
+  // for (int i = 1; i <= 100; i++) {
+  //   print(i);
+  // }
+
+  // for (int a = 10; a > 0; a--) {
+  //   print(a);
+  // }
+
+  // for (int i = 0; i < 10; i++) {
+  //   print("Abdulla Avloniy");
+  // }
+
+  // int umumiy = 0;
+  // print(umumiy);
+  // for (int i = 1; i <= 100; i++) {
+  //   umumiy += i;
+  // }
+  // print(umumiy);
+
+  // for (int i = 0; i <= 100; i++) {
+  //   if (i % 2 == 0) {
+  //     print(i);
+  //   }
+  // }
+
+  // for (int i = 1; i <= 100; i += 2) {
+  //   print(i);
+  // }
+
+  for (int i = 1; i >= 1; i++) {
+    print(i);
+  }
+}
