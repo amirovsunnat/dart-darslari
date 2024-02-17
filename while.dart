ab2c096d25a0011928e0cb4bff7c4cf404a3f569
Sunnat Amirov <sunnatbekamirov22@gmail.com>
2024-02-17 09:06:06 +0500

    While loop.

---
+++ while.dart
@@ -0,0 +1,43 @@
+void main(List<String> args) {
+  // for (int i = 1; i <= 10; i++) {
+  //   print(i);
+  // }
+
+  // int a = 1;
+  // while (a <= 10) {
+  //   print(a);
+  //   a++;
+  // }
+
+  // int b = 10;
+  // while (b >= 1) {
+  //   // b ning qiymati 1 dan katta yoki tengmi?
+  //   print(b);
+  //   b--; // b-1 : b dan 1 ni ayiramiz
+  // }
+
+  // int umumiy = 0;
+  // int n = 100;
+  // int c = 0;
+
+  // while (c <= n) {
+  //   // umumiy = umumiy + c;
+  //   umumiy += c;
+  //   c++;
+  // }
+  // // print(umumiy);
+  // print("1 dan 100 gacha bo'lgan sonlarni yig'indisi $umumiy");
+
+  int d = 1;
+  // while (d <= 44) {
+  //   if (d % 2 == 0) {
+  //     print(d);
+  //   }
+  //   d++;
+  // }
+
+  while (d <= 44) {
+    print(d);
+    d += 2;
+  }
+}
